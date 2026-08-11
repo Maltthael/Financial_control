8d3a55e1d6ea8f1fb97612bec15c5b679feb39a4
Maltthael <macrodrigues01@hotmail.com>
2026-08-11 16:49:33 -0300

    style: new visual for login page

--- FrontEnd/src/features/login_user/Login.jsx
+++ FrontEnd/src/features/login_user/Login.jsx
@@ -1,6 +1,7 @@
 import { useAuth } from './AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
+import './login.css';
 
 function Login() {
 
@@ -61,48 +62,72 @@ function Login() {
     };
 
     return (
-        <div>
-            <h1>{precisa2FA ? "Autenticação de Dois Fatores" : "Login"}</h1>
-            <form onSubmit={handleLogin}>
-                {!precisa2FA ? (
-                    <>
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                        <input
-                            type="password"
-                            placeholder="Senha"
-                            value={senha}
-                            onChange={(e) => setSenha(e.target.value)}
-                            required
-                        />
-                        <button type="submit">Entrar</button>
-                    </>
-                ) : (
-                    <>
-                        <p>Digite o código de 6 dígitos do seu aplicativo autenticador:</p>
-                        <input
-                            type="text"
-                            maxLength="6"
-                            placeholder="000000"
-                            value={codigo2FA}
-                            onChange={(e) => setCodigo2FA(e.target.value)}
-                            required
-                        />
-                        <button type="submit">Verificar Código</button>
-                    </>
-                )}
-                <br />
-                
-                    <Link style={{marginRight: "1%"}} to="/cadastro">Não possui uma conta? </Link>
-                    <Link to="/recuperar_senha"> Esqueceu a senha?</Link>
 
-            </form>
+
+
+        <div className='login-body'>
+            <div className='login-container'>
+                <div className='login-content'>
+                    <h1 className='login-content-title'>Administre sua vida financeira</h1>
+                    <p className='login-content-text'>Conecte-se com a financial control para um dinheiro mais saudavel.</p>
+                </div>
+
+            </div>
+
+            <div className='login-container2'>
+                <form onSubmit={handleLogin} className='login-form'>
+                    <div >
+                        {!precisa2FA ? (
+                            <>
+                                <input className='login-input'
+                                    type="email"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                                <input className='login-input'
+                                    type="password"
+                                    placeholder="Senha"
+                                    value={senha}
+                                    onChange={(e) => setSenha(e.target.value)}
+                                    required
+                                />
+                                <button type="submit" className='login-button'>Entrar</button>
+                            </>
+                        ) : (
+                            <>
+                             <h1>{precisa2FA ? "Autenticação de Dois Fatores" : "Login"}</h1>
+                                <br/>
+                                <p>Digite o código de 6 dígitos do seu aplicativo autenticador:</p>
+                                <br/>
+                                <input className='login-input'
+                                    type="text"
+                                    maxLength="6"
+                                    placeholder="000000"
+                                    value={codigo2FA}
+                                    onChange={(e) => setCodigo2FA(e.target.value)}
+                                    required
+                                />
+                                <button type="submit" className='login-button'>Verificar Código</button>
+                            </>
+                        )}
+                    </div>
+
+                    <br />
+                    <div className='login-container-links'>
+                        <Link className='login-links' to="/cadastro"> Não possui uma conta?  </Link>
+                        <Link className='login-links' to="/recuperar_senha"> Esqueceu a senha? </Link>
+                    </div>
+
+
+                </form>
+            </div>
         </div>
+      
+
+
+
     );
 };
 
